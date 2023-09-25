119c19bad3eb79f0c7123d30d36e9cae74c434cd
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-09-25 20:21:24 +0300

    Update product.py

--- Application/product.py
+++ Application/product.py
@@ -11,6 +11,13 @@ class ProductDetails:
     # Create an ID for the product.
     self.product = new_product_id
 
+  def delete_product(self, quantity):
+    # Remove stock from the product.
+    if self.stock_quantity >= quantity:
+      self.stock_quantity -= quantity
+    else:
+      print("Insufficient stock")
+
   def update_product_price(self, new_price):
     # Update the price of the product units.
     self.unit_price = new_price
@@ -19,16 +26,13 @@ class ProductDetails:
     # Retrieve the ID of the product.
     self.product_id = product_id
 
-  def update_product_stock(self, quantity):
-    # Update the stock of the product.
+  def add_product_stock(self, quantity):
+    # Update the stock of the product (addition).
     self.stock_quantity += quantity
 
-  def delete_product(self, quantity):
-    # Remove stock from the product.
-    if self.stock_quantity >= quantity:
-      self.stock_quantity -= quantity
-    else:
-      print("Insufficient stock")
+  def delete_product_stock(self, quantity):
+    # Update the stock of the product (subtraction).
+    self.stock_quantity -= quantity
 
   def update_product_status(self, status):
     # Update the status of the product.
@@ -37,17 +41,15 @@ class ProductDetails:
   def update_product_description(self, new_description):
     # Update the description of the product.
     self.description = new_description
-          
-  def mark_as_unavailable(self):
-    # Mark the product as unavailable for purchase.
-    self.is_available = False
 
   def mark_as_available(self):
     # Mark the product as available for purchase.
     self.is_available = True
+          
+  def mark_as_unavailable(self):
+    # Mark the product as unavailable for purchase.
+    self.is_available = False
   
-
-
   def get_product_info(self):
     # Get information about the product.
     product_info = f"Product ID: {self.product_id}\n"
