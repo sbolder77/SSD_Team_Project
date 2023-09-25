31a5975c237ac823fa20f616ee6ce179f12026f1
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-09-25 20:38:08 +0300

    Update users.py

--- Application/users.py
+++ Application/users.py
@@ -9,7 +9,7 @@ jsonData = data["Users"]
 class UserDetails:
     def __init__(self):
         self.test = "test"
-
+        
     def get_user(self, user_name):
         #fernet = Fernet(key)
         #with open(user_data_file, 'rb') as f:
@@ -61,7 +61,7 @@ class UserDetails:
             "County": self.county,
             "Postcode": self.postcode
         })
-        with open (user_data_file, 'w') as json_file:
+        with open (user_data_file, 'w') as users.json:
             json.dump(data, json_file, indent=4, separators=(',',': '))
         #with open(user_data_file, 'rb') as f:
             #data = f.read()
@@ -71,6 +71,43 @@ class UserDetails:
         #return 'User created'
     
     def edit_user(self):
+        return u.get_user(user_name)
+        if u.get_user(user_name) == True:
+
+            self.id = userlist[0]
+            self.firstName = userlist[1]
+            self.surname = userlist[2]
+            self.password = userlist[3]
+            self.emailaddress = userlist[4]
+            self.house = userlist[5]
+            self.street = userlist[6]
+            self.town = userlist[7]
+            self.county = userlist[8]
+            self.postcode = userlist[9]
+            jsonData.append({
+                "ID": self.id,
+                "Role": 'U',
+                "FirstName": self.firstName,
+                "Surname": self.surname,
+                "Password": self.password,
+                "EmailAddress": self.emailaddress,
+                "House": self.house,
+                "Street": self.street,
+                "Town": self.town,
+                "County": self.county,
+                "Postcode": self.postcode
+            })
+            with open (user_data_file, 'w') as users.json:
+                json.dump(data, json_file, indent=4, separators=(',',': '))
+
+
+    def check_user(user_name):
+        if bool(u.get_user(user_name)) = True:
+            print('Welcome back. Logging you in...')
+        else:
+            print('Account not found. Please register an account to log in.')
+
+    def authorise_user(user_name):
         return self.test
 
     def delete_user(self):
