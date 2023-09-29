2453cc5535a2f9fa52e1f8f1a219526bd17920d2
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-09-29 21:10:00 +0300

    Update logging.py

--- Application/logging.py
+++ Application/logging.py
@@ -3,8 +3,8 @@ class LoggingDetails:
         self.test = "test"
         
     def logger(level, message):
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    file_object = open('logfile.log', 'a')
-    file_object.write(level + ',' + dt_string + ',' + message + '\n')
-    file_object.close()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        file_object = open('logfile.log', 'a')
+        file_object.write(level + ',' + dt_string + ',' + message + '\n')
+        file_object.close()
