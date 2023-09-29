db39fde7fde437981e039045ef2291618dc3d005
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-09-29 20:01:30 +0300

    Update users.py

--- Application/users.py
+++ Application/users.py
@@ -12,10 +12,20 @@ class User:
 
     def create_user():
         user_data = []
-        with open (jsonData_users, "r") as f:
+        with open (jsonData_users, "w") as f:
             temp = json.load(f)
         user_data["password"] = input("Password: ")
+        
         user_data["username"] = input("Username: ")
+        if username not in jsonData_users:
+            pass
+        else:
+            print("This username is not available.")
+            choice_query = input("Please input another username: ")
+            if choice_query == 1:
+                    continue
+                else:
+                    break    
         user_data["user_id"] = input("User ID: ")
         user_data["firstName"] = input("First Name: ")
         user_data["surname"] = input("Surname: ")
@@ -57,12 +67,66 @@ class User:
                 print("\n\n")
     
     def edit_user():
-        pass
+        view_user()
+        with open (jsonData_users, "w") as f:
+            temp = json.load(f)
+            choice_query = input("Input which of your user details you wish to edit (if you wish to exit, input 'exit'): ")
+            if choice_query == str("Password")
+                user_data["password"] = input("Password: ")
+                print("Password changed.")
+                continue
+                
+            elif choice_query == str("Username"):
+                user_data["username"] = input("Username: ")
+                print("Username changed.")
+                continue
 
+            elif choice_query == str("User ID"]
+                print("User ID is unchangeable.")
+                continue
+                
+            elif choice_query == str("First Name"):
+                user_data["firstname"] = input("First Name: ")
+                print("First Name changed.")
+                continue
+
+            elif choice_query == str("Surname"):
+                user_data["surname"] = input("Surname: ")
+                print("Surname changed.")
+                continue
+
+           elif choice_query == str("Email Address"):
+                user_data["email_address"] = input("Email Address: ")
+                print("Email Address changed.")
+                continue
+
+           elif choice_query == str("House Number"]
+                user_data["house_number"] = input("House Number")
+                print("House Number changed.")
+                continue
+
+           elif choice_query == str("Street"]
+                user_data["street"] = input("Street")
+                print("Street changed.")
+                continue
+
+           elif choice_query == str("Town"]
+                user_data["town"] = input("Town")
+                print("Town changed.")
+                continue
+
+           elif choice_query == str("Postcode"]
+                user_data["psotcode"] = input("Postcode")
+                print("Postcode changed.")
+                continue
+
+            else:
+                break
+                
     def delete_user():
         User.view_user()
         new_user = []
-        with open (jsonData_users, "r") as f:
+        with open (jsonData_users, "w") as f:
             temp = json.load(f)
         delete_query = input("Do you wish to delete your user account (input 'Yes' or 'No'?")
         for entry in temp:
