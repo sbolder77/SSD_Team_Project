ff200f8c00267a4501f60c005ea7380cb15add8f
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-09-30 18:06:19 +0300

    Update onlineretailer.py

--- Application/onlineretailer.py
+++ Application/onlineretailer.py
@@ -1,18 +1,8 @@
-from users import create_user
-from users import edit_user
-from users import delete_user
-from user import authorise_user
-from user import load_user
-
-from orders import create_order
-from orders import view_order
-from orders import edit_order
-from orders import delete_order
-
-from items import create_item
-from items import view_item
-from items import edit_item
-from items import delete_item
+import users
+import items
+import orders
+import settings
+import logging
     
 def main():
     login_option = str(input('Choose a following option - 'R' = Register A User Account or 'L' = Login: '))
