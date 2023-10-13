a23888fe8027b89c56b7fede63318a17030a7404
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-10-13 21:40:08 +0200

    Update items.py

--- Application/items.py
+++ Application/items.py
@@ -1,7 +1,7 @@
 import json
 
 class Item():
-    with open('items.json') as f:    
+    with open('items.json', encoding='utf-8') as f:    
         item_data = json.load(f)
     
     def __init__(self):
@@ -16,8 +16,9 @@ class Item():
             "itemStock": itemStock_query
         }
         Item.item_data['items'].append(item)
-        with open('items.json', 'w') as f:
+        with open('items.json', 'w', encoding='utf-8') as f:
             json.dump(Item.item_data, f, indent=4, separators=(',', ': '))
+            print('')
             print("Item created.")
 
     def view_item(self):
@@ -55,28 +56,10 @@ class Item():
     def search_itemPrice(self, itemPrice_query):
         _price = itemPrice_query
         for i in Item.item_data['items']:
-            if i['itemPrice'] == _price:
+            if int(i['itemPrice']) == _price:
                 print('')
                 print('The item you searched for...')
                 print(f"\nID:{i['itemID']} \nName:{i['itemName']} \nPrice:{i['itemPrice']} \nDescription:{i['itemDescription']} \nStock:{i['itemStock']}")
-
-    def edit_item(self, itemID_query, itemName_query, itemPrice_query, itemDescription_query, itemStock_query, new_itemID, new_itemName, new_itemPrice, new_itemDescription, new_itemStock):
-        for i in Item.item_data['items']:
-            if i['itemID'] == itemID_query:
-                i['itemID'] == new_itemID
-            elif i['itemName'] == itemName_query:
-                i['itemName'] == new_itemName
-            elif i['itemPrice'] == itemPrice_query:
-                i['itemPrice'] == new_itemPrice
-            elif i['itemDescription'] == itemDescription_query:
-                i['itemID'] == new_itemDescription
-            elif i['itemStock'] == itemStock_query:
-                i['itemStock'] == new_itemStock
-            else:
-                print("Cannot be found.")
-        with open('items.json', 'w') as f:
-            json.dump(Item.item_data, f, indent=4, separators=(',', ': '))
-            print(f"Update made.")
 
     def delete_item(self, itemID_query):
         _ID = itemID_query
@@ -85,10 +68,12 @@ class Item():
             if i['itemID'] == _ID:
                 Item.item_data['items'].pop(Item.item_data['items'].index(i))
                 finder = True
-                print(f"{_ID} deleted.")
+                print('')
+                print(f"Item with ID {_ID} deleted.")
                 break
-        with open('items.json', 'w') as f:
+        with open('items.json', 'w', encoding='utf-8') as f:
             json.dump(Item.item_data, f, indent=4, separators=(',', ': '))
         if finder == False:
-            print(f"{_ID} could not be found.")
+            print('')
+            print(f"Item with ID {_ID} could not be found.")
             i=i+1
