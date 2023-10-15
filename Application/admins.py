acb1dfff54a9cc01ea9b609d454971d6cac40434
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-10-15 11:30:56 +0200

    Update admins.py

--- Application/admins.py
+++ Application/admins.py
@@ -1,19 +1,24 @@
 #Importing necessary modules
 from cryptography.fernet import Fernet
 
+#Defining Admin() class and it's downstream functions
 class Admin():
-    def write_administratorDeposit(self, adminAuthenticator_1):
+#Defining writing administrator authentication deposit file
+    def write_administratorDeposit(self, adminAuthenticator):
         with open("adminKey.csv", "w", encoding='utf-8') as file:
-            file.write(adminAuthenticator_1)
-    
+            file.write(adminAuthenticator)
+            
+#Defining generating of administrator key and writing it to a file
     def write_adminKey(self):
         key = Fernet.generate_key()
         with open ("key.adminKey", "wb") as key_file:
             key_file.write(key)
-    
+
+#Defining loading of admin key
     def load_adminKey(self):
         return open("key.adminKey", "rb").read()
 
+#Defining of encrypting input from user, writing it to a file & returning it
     def encrypt(self, filename, key):
         f = Fernet(key)
         with open(filename, "rb") as file:
@@ -23,6 +28,7 @@ class Admin():
             file.write(encrypted_password)
         return encrypted_password
 
+#Defining of decrypting input from user, writing it to a file & returning it
     def decrypt(self, filename, key):
         f = Fernet(key)
         with open(filename, "rb") as file:
