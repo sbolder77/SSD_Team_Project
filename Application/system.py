88ad62e285497d4d0a503798d8e8881825ee09e4
lliamwillson <114589290+lliamwillson@users.noreply.github.com>
2023-10-15 14:26:38 +0200

    Update system.py

--- Application/system.py
+++ Application/system.py
@@ -1,25 +1,27 @@
 #https://www.geeksforgeeks.org/encrypt-and-decrypt-files-using-python/
 #https://regex-generator.olafneumann.org
 
+#Importing necessary modules
 import json
 from cryptography.fernet import Fernet
 import filelogging
 from datetime import datetime
 import re
 
-# create json readable object
+#Create json readable object
 filename = 'system.json'
 f = open(filename)
 data = json.load(f)
 
-#region variables
+#Variables
 fernet = Fernet('d35AFvCPEXATL7kNnjz6CD6r20KR9qh5q1L9nZ6bk5k=')
 USER_NON_ALPHA = re.compile('[A-Z]+_[A-Z]')
 ITEM_ALPHA = re.compile('[^A-Z0-9]+')
 ORDER_ALPHA = re.compile('[^A-Z0-9]+')
-#endregion
 
+#Defining Settings() class and downstream functions
 class Settings():
+#Defining __init__
     def __init__(self):
         for x in data:
             self.log = x['log']
@@ -28,6 +30,7 @@ class Settings():
             self.encrypt = x['encrypt']
             self.useregex = x['useregex']
 
+#Defining file encryption functionality
     def encryptfile(self, filename, user):
         l = filelogging.LoggingDetails()
         if self.encrypt == 'TRUE':
@@ -39,7 +42,7 @@ class Settings():
             l.write_system_log("SYSTEM", "INFO", filename + " has been encrypted", user)
             return filename + " has been encrypted"
 
-
+#Defining file decryption functionality
     def decryptfile(self, filename, user):
         l = filelogging.LoggingDetails()
         if self.encrypt == 'TRUE':
@@ -50,8 +53,8 @@ class Settings():
                 decrypted_file.write(decrypted)
             l.write_system_log("SYSTEM", "INFO", filename + " has been decrypted", user)
             return filename + " has been decrypted"
-        
 
+#Defining user regex functionality
     def regex_user(self, user):
         if self.useregex == 'TRUE':
             user = USER_NON_ALPHA.sub('', user.upper())
@@ -59,7 +62,8 @@ class Settings():
                 return "Valid User"
             else:
                 return "Invalid User"
-            
+
+#Defining item regex functionality
     def regex_item(self, item):
         if self.useregex == 'TRUE':
             item = USER_NON_ALPHA.sub('', item.upper())
@@ -67,7 +71,8 @@ class Settings():
                 return "Valid Item"
             else:
                 return "Invalid Item"
-            
+
+#Defining order regex functionality
     def regex_order(self, order):
         if self.useregex == 'TRUE':
             order = USER_NON_ALPHA.sub('', order.upper())
@@ -75,6 +80,7 @@ class Settings():
                 return "Valid Order"
             else:
                 return "Invalid Order"
-                
+
+#Defining logging functionality
     def logging(self):
         return self.log
